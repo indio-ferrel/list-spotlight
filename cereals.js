b31795b36346fe24736f134a696e2e5c85a3a787
Indio Ferrel <mferrel1911@gmail.com>
2022-05-05 16:14:05 -0700

    Implemented first list object (cereals) in its own file (cereals.js)

---
+++ cereals.js
@@ -0,0 +1,8 @@
+export const cereals = [
+    { name: 'Lucky Charms', health: 1, deliciousness: 5 },
+    { name: 'Honey Bunches of Oats', health: 5, deliciousness: 0 },
+    { name: 'Cunnamon Toast Crunch', health: 2, deliciousness: 4 },
+    { name: 'Fruit Loops', health: 0, deliciousness: 3 },
+    { name: 'Rice Krispies', health: 4, deliciousness: 1 },
+    { name: 'Apple Jacks', health: 3, deliciousness: 1 },
+];
